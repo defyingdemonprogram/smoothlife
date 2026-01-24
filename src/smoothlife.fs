b9d798638aae80268104bfb692c445fe765cf902
sanjeev.bhandari <sanjeev.bhandari@tai.com.np>
2026-01-25 01:09:58 +0545

    refactor: Remove unused `fragColor` and `colDiffuse` variables from the smoothlife fragment shader.

--- src/smoothlife.fs
+++ src/smoothlife.fs
@@ -2,12 +2,9 @@
 
 // Input vertex attributes (from vertex shader)
 in vec2 fragTexCoord;
-in vec4 fragColor;
 
 // Input uniform values
 uniform sampler2D texture0;
-uniform vec4 colDiffuse;
-
 // Output fragment color
 out vec4 finalColor;
 
